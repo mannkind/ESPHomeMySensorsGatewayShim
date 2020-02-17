96d086454687e32e7235f74397c012ca59ee1f63
Dustin Brewer <mannkind@thenullpointer.net>
2020-02-17 19:22:29 +0000

    Attempting to update espressif/MySensors versions

--- ESPHomeMySensorsGatewayShim.h
+++ ESPHomeMySensorsGatewayShim.h
@@ -18,14 +18,17 @@ class MySensorsGatewayShim : public Component, public CustomMQTTDevice
   public:
     void setup() override
     {
-        this->subscribe(this->MySensorsBootloaderRegisterTopic(), &MySensorsGatewayShim::MySesnorsBootloaderRegisterCallback);
+        this->subscribe(this->MySensorsBootloaderRegisterTopic(), &MySensorsGatewayShim::MySensorsBootloaderRegisterCallback);
         this->subscribe(this->MySensorsBootloaderIDResponseTopic(), &MySensorsGatewayShim::MySensorsBootloaderIDResponseCallback);
     }
 
-    void MySesnorsBootloaderRegisterCallback(const std::string &topic, const std::string &payload) 
+    void MySensorsBootloaderRegisterCallback(const std::string &topic, const std::string &payload) 
     {
-        ESP_LOGD(TAG, "Received Register - Payload: %s", payload.c_str());
-        const auto nodeId = atoi(payload.c_str());
+        ESP_LOGD(TAG, "Received Register - Topic: %s", topic.c_str());
+        MyMessage msg;
+        protocolMQTT2MyMessage(msg, const_cast<char*>(topic.c_str()), reinterpret_cast<uint8_t*>(const_cast<char*>(payload.c_str())),  payload.length());
+        auto nodeId = msg.getDestination();
+        ESP_LOGD(TAG, "Received Register - NodeId: %d", nodeId);
 
         auto found = false;
         for (auto i = 0; i < this->nodes.size(); i++) {
@@ -54,7 +57,7 @@ class MySensorsGatewayShim : public Component, public CustomMQTTDevice
         ESP_LOGD(TAG, "Received ID Response - Topic: %s, Payload: %s", topic.c_str(), payload.c_str());
 
         MyMessage msg;
-        protocolMQTTParse(msg, topic.c_str(), payload.c_str(),  payload.length());
+        protocolMQTT2MyMessage(msg, const_cast<char*>(topic.c_str()), reinterpret_cast<uint8_t*>(const_cast<char*>(payload.c_str())),  payload.length());
         transportRouteMessage(msg);
     }
 
@@ -66,7 +69,7 @@ class MySensorsGatewayShim : public Component, public CustomMQTTDevice
         ESP_LOGD(TAG, "Received CF Response - Topic: %s, Payload: %s", topic.c_str(), payload.c_str());
 
         MyMessage msg;
-        protocolMQTTParse(msg, topic.c_str(), payload.c_str(), payload.length());
+        protocolMQTT2MyMessage(msg, const_cast<char*>(topic.c_str()), reinterpret_cast<uint8_t*>(const_cast<char*>(payload.c_str())),  payload.length());
         transportRouteMessage(msg);
     }
 
@@ -101,8 +104,8 @@ class MySensorsGatewayShim : public Component, public CustomMQTTDevice
      */
     const std::string MySensorsBootloaderRegisterTopic() const
     {
-        char buf[sizeof(MY_MQTT_SUBSCRIBE_TOPIC_PREFIX) + 10];
-        sprintf(buf, "%s/%s", MY_MQTT_SUBSCRIBE_TOPIC_PREFIX, "register");
+        char buf[sizeof(MY_MQTT_PUBLISH_TOPIC_PREFIX) + 20];
+        sprintf(buf, "%s/+/255/4/0/0", MY_MQTT_PUBLISH_TOPIC_PREFIX);
         return std::string(buf);
     }
 
@@ -121,115 +124,10 @@ void receive(const MyMessage &_message)
 
     // Default MySensors node handling
     char _buf[MAX_PAYLOAD * 2 + 1];
-    const auto topic = protocolFormatMQTTTopic(MY_MQTT_PUBLISH_TOPIC_PREFIX, message);
+    const auto topic = protocolMyMessage2MQTT(MY_MQTT_PUBLISH_TOPIC_PREFIX, message);
     const auto msg = message.getString(_buf);
     const auto retain = mGetCommand(message) == C_SET || 
         (mGetCommand(message) == C_INTERNAL && message.type == I_BATTERY_LEVEL);
 
     mqtt::global_mqtt_client->publish(topic, msg, strlen(msg), 0, retain);
-}
-
-/* 
- * protocolMQTTParse taken from MySensors MyProtocolMySensors.cpp w/minor modifications.
- * Requires #define MY_GATEWAY_MQTT_CLIENT which brings with it a lot of baggage.
- * 
- * Original copyright notice from MyProtocolMySensors.cpp
- * 
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2018 Sensnology AB
- * Full contributor list: https://github.com/mysensors/MySensors/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- */
-bool protocolMQTTParse(MyMessage &message, const char *topic, const char *payload, unsigned int length)
-{
-    char *str, *p;
-    uint8_t i = 0;
-    uint8_t command = 0;
-    char *tt = const_cast<char *>(topic) + strlen(MY_MQTT_SUBSCRIBE_TOPIC_PREFIX) + 1;
-
-    for (str = strtok_r(tt, "/", &p); str && i <= 5; str = strtok_r(NULL, "/", &p))
-    {
-        switch (i)
-        {
-        case 0:
-        {
-            // Node id
-            message.destination = atoi(str);
-            break;
-        }
-        case 1:
-        {
-            // Sensor id
-            message.sensor = atoi(str);
-            break;
-        }
-        case 2:
-        {
-            // Command type
-            command = atoi(str);
-            mSetCommand(message, command);
-            break;
-        }
-        case 3:
-        {
-            // Ack flag
-            mSetRequestAck(message, atoi(str) ? 1 : 0);
-            break;
-        }
-        case 4:
-        {
-            // Sub type
-            message.type = atoi(str);
-            break;
-        }
-        }
-        i++;
-    }
-
-    if (i != 5)
-    {
-        return false;
-    }
-
-    message.sender = GATEWAY_ADDRESS;
-    message.last = GATEWAY_ADDRESS;
-    mSetAck(message, false);
-
-    // Add payload
-    if (command == C_STREAM)
-    {
-        uint8_t bvalue[MAX_PAYLOAD];
-        uint8_t blen = 0;
-        while (*payload)
-        {
-            uint8_t val;
-            val = protocolH2i(*payload++) << 4;
-            val += protocolH2i(*payload++);
-            bvalue[blen] = val;
-            blen++;
-        }
-        message.set(bvalue, blen);
-    }
-    else
-    {
-        char *ca;
-        ca = (char *)payload;
-        ca += length;
-        *ca = '\0';
-        message.set((const char *)payload);
-    }
-
-    return true;
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ESPHomeMySensorsGatewayShim
 
-A barebones shim to enable an ESPHome device to function as a MySensors MQTT Gateway. Tested with ESPHome 1.13.x, MySensors 2.3.1, and a Wemos D1 Mini w/an RF24 Radio.
+A barebones shim to enable an ESPHome device to function as a MySensors MQTT Gateway. Tested with ESPHome 1.13.x, MySensors 2.3.2, and a Wemos D1 Mini w/an RF24 Radio.
 
 ## Setup/Use
 
